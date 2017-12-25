69d425aa35cf43935c13dd9e0e1d2e5c42c836f9
ahasani <albin.hasani@ikubinfo.al>
2017-12-25 01:13:44 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using movingtotheweb;
+using Microsoft.AspNetCore.Hosting;
+
+namespace supermva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
