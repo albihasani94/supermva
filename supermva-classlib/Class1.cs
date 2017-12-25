0125a90a0cf1d6cb274d580971facb57d740112e
ahasani <albin.hasani@ikubinfo.al>
2017-12-25 19:46:35 +0100

    add class library/nuget package

---
+++ supermva-classlib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace supermva_classlib
+{
+    public class Class1
+    {
+        public static string Greeting()
+        {
+            return "Hello from the class library";
+        }
+    }
+}
