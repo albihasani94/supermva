dc2bc4a4f4af39079788f48144686381a8cd59a1
ahasani <albin.hasani@ikubinfo.al>
2017-12-26 23:55:39 +0100

    add constraints to model

--- musicstore/Models/Album.cs
+++ musicstore/Models/Album.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace musicstore.Models
 {
@@ -6,9 +7,14 @@ namespace musicstore.Models
     {
         public int ID { get; set; }
         public string Artist { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'' - '\s]*$")]
         public string Genre { get; set; }
         public DateTime ReleaseDate { get; set; }
+        [Required]
+        [Range(0, 100.00)]
         public decimal Price { get; set; }
     }
 }
