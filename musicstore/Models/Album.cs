219b52ce24a15702e778bf709e51219185ef46a4
ahasani <albin.hasani@ikubinfo.al>
2017-12-26 23:03:39 +0100

    add album model

---
+++ musicstore/Models/Album.cs
@@ -0,0 +1,12 @@
+namespace musicstore.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
