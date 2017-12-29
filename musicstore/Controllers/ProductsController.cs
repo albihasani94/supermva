c61b0f64001d1fb7dd93f902ac089fc337687047
ahasani <albin.hasani@ikubinfo.al>
2017-12-29 22:44:12 +0100

    add api methods

--- musicstore/Controllers/ProductsController.cs
+++ musicstore/Controllers/ProductsController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using musicstore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace musicstore.Controllers
@@ -6,6 +10,40 @@ namespace musicstore.Controllers
     [Route("api/Products")]
     public class ProductsController : Controller
     {
-        public string Get() => "Hello API";
+        private static List<Product> _products = new List<Product>(new[]
+            {
+                new Product() {ID = 1, Name = "Test"},
+                new Product() {ID = 2, Name = "_test2"}
+            }
+        );
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return _products;
+        }
+
+        [HttpGet("{id}")] // capture route parameter
+        public IActionResult Get(int id)
+        {
+            var product = _products.SingleOrDefault(p => p.ID == id);
+
+            return product == null ? (IActionResult) this.NotFound() : Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+//            if (product == null) throw new ArgumentNullException(nameof(product));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _products.Add(product);
+            return CreatedAtAction(actionName: nameof(Get),
+                routeValues: new {id = product.ID}, value: product);
+        }
     }
 }
--- musicstore/Models/Product.cs
+++ musicstore/Models/Product.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace musicstore.Models
 {
     public class Product
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
         public int Quantity { get; set; }
     }
